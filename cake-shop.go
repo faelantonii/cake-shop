00c8dbe6f47a7aaec496e1dd603e8d201242f932
Fael Antoni <faelantonii@gmail.com>
2024-04-25 15:09:01 +0700

    create code

--- cake-shop.go
+++ cake-shop.go
@@ -5,21 +5,38 @@ import (
 )
 
 func main() {
+	// Data pesanan dalam setiap batch
 	order := map[string][]string{
 		"batch1": {"brownies", "nastar", "muffin", "muffin", "brownies", "pie", "nastar", "muffin"},
 		"batch2": {"brownies", "pie", "pie", "nastar"},
 	}
 
-	for batch, items := range order {
-		fmt.Println("Processing", batch)
+	// Simpan batch
+	batchNames := make([]string, 0, len(order))
+	for batch := range order {
+		batchNames = append(batchNames, batch)
+	}
+
+	// Iterasi setiap nama batch dan print pesanan
+	for _, batch := range batchNames {
+		// print pesan bahwa sedang memproses batch
+		fmt.Println("Processing ", batch)
+
+		// Ambil daftar pesanan dalam batch
+		items := order[batch]
+
+		// Hitung jumlah setiap jenis pesanan dalam batch
 		counts := make(map[string]int)
 		for _, item := range items {
 			counts[item]++
 		}
 
+		// Iterasi setiap jenis pesanan dan print jumlahnya
 		for item, count := range counts {
 			fmt.Printf("%d %s is ready!\n", count, item)
 		}
+
+		// print baris kosong buat spasi aja
 		fmt.Println()
 	}
 }
